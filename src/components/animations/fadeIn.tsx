069463da39f4d8dae18d96aa1599a35d18f3cf52
paolochmprojects <paolo.dev.projects@gmail.com>
2024-05-04 18:44:04 -0500

    [ADD] framer motion

---
+++ src/components/animations/fadeIn.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { motion, type Variants } from 'framer-motion'
+import React from 'react'
+
+interface Props {
+    children: React.ReactNode
+}
+
+const container: Variants = {
+    hidden: {
+        opacity: 0,
+        scale: 1,
+        translateX: -100,
+    },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        translateX: 0
+    }
+}
+
+const FadeIn = ({ children }: Props) => {
+    return (<motion.div variants={container} initial="hidden" animate="visible">
+        {children}
+    </motion.div>)
+}
+
+export default FadeIn
