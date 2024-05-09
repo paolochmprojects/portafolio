fa88f5d31f7c2afe71e30e6aefe57950f4892257
paolochmprojects <paolo.dev.projects@gmail.com>
2024-05-09 17:44:23 -0500

    [DELETE] console.log delete

--- src/components/navlink.tsx
+++ src/components/navlink.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React from 'react'
 
 interface Props {
@@ -11,13 +10,11 @@ interface Props {
 
 const NavLink = ({ name, url, close }: Props) => {
 
-    const router = usePathname()
-    console.log(router)
     return (
-        <Link 
-        onClick={close}
-        className='hover:text-red-600 active:text-red-600 text-sm font-medium transition-all duration-500 hover:scale-110'
-        href={url}>{name}</Link>
+        <Link
+            onClick={close}
+            className='hover:text-red-600 active:text-red-600 text-sm font-medium transition-all duration-500 hover:scale-110'
+            href={url}>{name}</Link>
     )
 }
 
